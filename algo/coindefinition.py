a355e9247a5ee9680d9a52e914e7ed568bb61f9a
ahmedbodi <ahmed_bodi@msn.com>
2014-07-14 04:09:55 +0100

    Add Myr Groestl Test 1

--- algo/coindefinition.py
+++ algo/coindefinition.py
@@ -3,7 +3,7 @@ import lib.settings as settings
 log = lib.logger.get_logger('Coin Definition')
 log.debug("Got to Coin Definition")
 
-ALGOS = {1:'ltc_scrypt', 2:None ,3:'yac_scrypt', 4:'quark_hash', 5:'x11_hash', 6:'algo.skeinhash.skeinhash', 7:'medcoin_hybrid', 8:'tjc_bcrypt'}
+ALGOS = {1:'ltc_scrypt', 2:None ,3:'yac_scrypt', 4:'quark_hash', 5:'x11_hash', 6:'algo.skeinhash.skeinhash', 7:'medcoin_hybrid', 8:'tjc_bcrypt', 9:'groestl_hash'}
 DIFF1 = {1:0x0000ffff00000000000000000000000000000000000000000000000000000000, 2:0x000000ffff000000000000000000000000000000000000000000000000000000, 3:0x00000000ffff0000000000000000000000000000000000000000000000000000, 4:0x001fff0000000000000000000000000000000000000000000000000000000000}
 # Algorithm Array is as follows:
 # Scrypt = 1
@@ -14,6 +14,7 @@ DIFF1 = {1:0x0000ffff00000000000000000000000000000000000000000000000000000000, 2
 # Skein = 6
 # HybridSHA256 = 7
 # tjcoin = 8
+# MYR Groestl = 0
 # Adding a new algo is as simple as editing lib/coindefinition.py and adding the algorithm to the array 
 
 class algo_needed:
@@ -34,6 +35,8 @@ class algo_needed:
 		self.algorithm = ALGOS[7]
 	  elif settings.ALGORITHM == 8:
 		self.algorithm = ALGOS[8]
+	  elif settings.ALGORITHM == 9:
+	  	self.algorithm == ALGOS[9]
 	  return self.algorithm
 
 
@@ -55,6 +58,8 @@ class diff1_needed:
          self.DIFF1 = DIFF1[1]
     elif settings.ALGORITHM == 8:
          self.DIFF1 = DIFF1[4]
+    elif settings.ALGORITHM == 9:
+    	 self.DIFF1 = DIFF1[4]
     return self.DIFF1
 
 class header_needed:
